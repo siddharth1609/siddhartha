9102dbb34b84c87bfb2e50d135725f4c8a538a9b
siddharth1609 <siddharthsonwane18@gmail.com>
2019-07-07 22:39:11 +0530

    error message class

---
+++ src/main/java/com/siddhartha/s/utils/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.siddhartha.s.utils;
+
+import java.sql.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(SiddharthException.class)
+	public final ResponseEntity<ErrorDetails> handleCustomTypeException(SiddharthException ex, WebRequest request) {
+
+		ErrorDetails details = new ErrorDetails(new Date(0), ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(details, HttpStatus.NOT_FOUND);
+
+	}
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(new Date(0), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
---
+++ src/main/java/com/siddhartha/s/utils/ErrorDetails.java
@@ -0,0 +1,20 @@
+package com.siddhartha.s.utils;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class ErrorDetails {
+	private Date timestamp;
+	private String message;
+	private String details;
+
+	public ErrorDetails(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+}
